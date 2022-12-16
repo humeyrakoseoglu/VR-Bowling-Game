effb833c114c3d70d2ccc37832f2d197189cb14f
humeyrakoseoglu <20180808015@ogr.akdeniz.edu.tr>
2022-12-16 20:38:35 +0300

    Photon update

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //base.OnJoinedLobby();
+        //PhotonNetwork.JoinOrCreateRoom("BowlingRoom", new RoomOptions { MaxPlayers = 4, IsOpen = true, IsVisible = true}, TypedLobby.Default);
+       // PhotonNetwork.JoinOrCreateRoom("oda1", new RoomOptions { MaxPlayers = 2, IsOpen = true, IsVisible = true },TypedLobby.Default);
+        SceneManager.LoadScene("Lobby");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/CreateAndJoinRoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+public class CreateAndJoinRoom : MonoBehaviourPunCallbacks
+{
+
+    public InputField createInput;
+    public InputField joinInput;
+
+    // Start is called before the first frame update
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    // Update is called once per frame
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("SampleScene");
+    }
+        
+    }
+
